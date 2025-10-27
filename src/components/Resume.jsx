a2433902647d40db77c67deb93bb934085246a7c
CHANG88S <140467098+CHANG88S@users.noreply.github.com>
2025-10-26 20:25:57 -0700

    Update Resume.jsx

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -5,9 +5,10 @@ export const Resume = () => {
             <h1 id="resume" className="text-3xl font-bold scroll-mt-20">Resume</h1>
         </div>
         
-        <iframe 
+        <iframe // Layout is still wrong for mobile, but good enough for now
+            aria-label="Embedded Resume PDF"
             src={`${import.meta.env.BASE_URL}resume.pdf#toolbar=0&navpanes=0`}
-            className="w-full h-[400px] md:h-[1000px] rounded-md" 
+            className="w-full h-[1000px] md:h-[1000px] rounded-md" 
             type="application/pdf" 
             style={{ border: 'none' }}
       />
